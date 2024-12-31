86f2798c5fb706c0a7bc97ef50c1e3f28e9a4b2b
dasamaf <haltriescode@gmail.com>
2024-12-31 20:49:03 +0700

    learn-python

---
+++ day-1/day-1.py
@@ -0,0 +1,26 @@
+print("Hello" + " World!")
+print("Hello" + " " + "World!")
+
+#Input
+input("What is your name?")
+print("Hello " + input("What is your name?") + "!")
+
+#Variables
+username = input("What is your name?")
+print(username)
+
+hi="Hi "
+print(hi + username)
+
+#length of string
+length = len(username)
+
+print(length)
+
+
+#Variables naming
+#1. Should not start with a number
+#2. Should not have spaces
+#3. Should not have special characters, except _
+#4. Should not have keywords, like print, input, len, etc.
+
