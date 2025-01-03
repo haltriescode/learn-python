b23637b7f690774dc123eb0e8fe9b6538bf35e36
dasamaf <haltriescode@gmail.com>
2025-01-03 07:18:02 +0700

    Day 5

---
+++ day-5/password-generator.py
@@ -0,0 +1,39 @@
+import random
+
+letter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbol = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+# Eazy Level - Order not randomised:
+# e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+password = ""
+for char in range(1, nr_letters + 1):
+    password += random.choice(letter)
+for char in range(1, nr_symbols + 1):
+    password += random.choice(symbol)
+for char in range(1, nr_numbers + 1):
+    password += random.choice(number)
+
+print(f"Your password is ${password}")
+
+# Hard Level - Order of characters randomised:
+# e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+password_list = []
+for char in range(1, nr_letters + 1):
+    password_list.append(random.choice(letter))
+for char in range(1, nr_symbols + 1):
+    password_list.append(random.choice(symbol))
+for char in range(1, nr_numbers + 1):
+    password_list.append(random.choice(number))
+
+random.shuffle(password_list)
+password = ""
+for char in password_list:
+    password += char
+
+print(f"or ${password}")
