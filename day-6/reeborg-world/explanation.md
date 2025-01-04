7e25d1e00714cf5f5d578d05c62aab2e1095bfa2
dasamaf <haltriescode@gmail.com>
2025-01-04 07:16:17 +0700

    day-6

--- day-6/reeborg-world/explanation.md
+++ day-6/reeborg-world/explanation.md
@@ -1,3 +1,3 @@
-Website: https://reeborg.ca/
+Website: https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json
 
 Apply the code on tasks.py to Reeborg's website.
--- day-6/reeborg-world/tasks.py
+++ day-6/reeborg-world/tasks.py
@@ -1,5 +1,6 @@
 # First Task
 # Make Reeborg turn right
+# web page: https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json
 
 def turn_right():
     turn_left()
@@ -8,6 +9,8 @@ def turn_right():
 
 # Second Task
 # Make Reeborg make a square
+# web page: https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json
+
 turn_left()
 move()
 turn_right()
@@ -20,7 +23,7 @@ move()
 # Third Task
 # Hurdle 1
 # Make Reeborg jump over the hurdle
-# Website: https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+# web page: https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
 
 def jump():
     move()
@@ -42,7 +45,7 @@ jump()
 # Fourth Task
 # Hurdle 2
 # Make Reeborg jump over the hurdles until it reaches the finish line
-# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%202&url=worlds%2Ftutorial_en%2Fhurdle2.json
+# web page: https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%202&url=worlds%2Ftutorial_en%2Fhurdle2.json
 
 while not at_goal(): # at_goal() is a function that returns True if Reeborg is at the goal, otherwise False
     jump()
@@ -50,7 +53,7 @@ while not at_goal(): # at_goal() is a function that returns True if Reeborg is a
 # Fifth Task
 # Hurdle 3
 # Make Reeborg jump over the hurdles. This time, Reebrorg will have to jump over the random placed hurdles until it reaches the finish line
-# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%203&url=worlds%2Ftutorial_en%2Fhurdle3.json
+# Website: https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%203&url=worlds%2Ftutorial_en%2Fhurdle3.json
 
 
 
