696ada18478f1274d4f8a1d4385f332c188df182
dasamaf <haltriescode@gmail.com>
2025-01-04 16:03:20 +0700

    day-6

--- day-6/reeborg-world/tasks.py
+++ day-6/reeborg-world/tasks.py
@@ -95,4 +95,28 @@ while not at_goal():
     else:
         move()
 
+# Seventh Task
+# Maze
+# Make Reeborg find the exit in the maze
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+def jump():
+    while wall_on_right():
+        move()
+    turn_right()
+    move()
+    turn_right()
+    
+    while front_is_clear():
+        move()
+    turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
 
