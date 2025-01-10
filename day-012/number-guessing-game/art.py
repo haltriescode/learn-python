7d785b043bf218ea13cd8cf3eb50d2c4ea2b9459
dasamaf <haltriescode@gmail.com>
2025-01-10 07:22:34 +0700

    improve game's ux

---
+++ day-012/number-guessing-game/art.py
@@ -0,0 +1,16 @@
+logo = '''
+                                                                                                         
+                                               88                                                                                                            
+                                               88                                                                                                            
+    8b,dPPYba, ,adPPYYba, 8b,dPPYba,   ,adPPYb,88  ,adPPYba,  88,dPYba,,adPYba,    
+    88P'   "Y8 ""     `Y8 88P'   `"8a a8"    `Y88 a8"     "8a 88P'   "88"    "8a  
+    88         ,adPPPPP88 88       88 8b       88 8b       d8 88      88      88           
+    88         88,    ,88 88       88 "8a,   ,d88 "8a,   ,a8" 88      88      88          
+    88         `"8bbdP"Y8 88       88  `"8bbdP"Y8  `"YbbdP"'  88      88      88
+                                           88
+8b,dPPYba,  88       88 88,dPYba,,adPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,
+88P'   `"8a 88       88 88P'   "88"    "8a 88P'    "8a a8P_____88 88P'   "Y8 
+88       88 88       88 88      88      88 88       d8 8PP""""""" 88
+88       88 "8a,   ,a88 88      88      88 88b,   ,a8" "8b,   ,aa 88 
+88       88  `"YbbdP'Y8 88      88      88 8Y"Ybbd8"'   `"Ybbd8"' 88        
+'''
--- day-012/number-guessing-game/main.py
+++ day-012/number-guessing-game/main.py
@@ -1,5 +1,7 @@
 import random
+from art import logo
 
+print(logo)
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
 hard_ease = input("Choose a difficulty. Type 'easy' or 'hard': ")
@@ -29,3 +31,4 @@ while attempt_point != 0:
         print("Guess Lower!")
         attempt_point -= 1
 
+print("You Lose!")
